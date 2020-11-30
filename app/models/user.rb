719f3643e9442a9d3da18b1c9eb78c9b4f1d7334
zerradi_amine <dasileker@gmail.com>
2020-11-30 14:24:06 +0100

    create the post & comment  model for the micro_reddit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
     validates :email, presence: true, confirmation: true
+
+    has_many :posts
+    has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_114746) do
+ActiveRecord::Schema.define(version: 2020_11_30_131914) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,4 +37,6 @@ ActiveRecord::Schema.define(version: 2020_11_30_114746) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
